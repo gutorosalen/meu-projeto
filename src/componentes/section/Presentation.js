7fc883b6820b3c1b19839f76c49ce425b22ea3fb
unknown <gutorosalen.gustavorosalen@hotmail.com>
2024-06-24 22:06:50 -0300

    v0.4.1

--- src/componentes/section/Presentation.js
+++ src/componentes/section/Presentation.js
@@ -1,7 +1,20 @@
+import styles from './Presentation.module.css'
+import ButtonA from './ButtonA'
+/*import ButtonB from './ButtonB'*/
+
 function Presentation() {
     return (
-        <div id='Presentation'>
-            <h1>Essa parte é minha Apresentação</h1>
+        <div className={styles.presentation} id='Presentation'>
+            <p><strong>Bem-vindo ao meu Portfólio</strong></p>
+            <h1>Olá, eu sou Gustavo</h1>
+            <p>
+                Sou um apaixonado por tecnologia e soluções inovadoras.<br />
+                Como Product Manager, eu tenho o compromisso de resolver problemas<br />
+                complexos e trazer resultados excepcionais para os negócios.<br />
+                Meus projetos já geraram milhões de reais em receita anual<br />
+                estou sempre em busca de novos desafios para superar.<br />
+            </p>
+            <ButtonA link='https://github.com/gutorosalen' text='Conecte-se comigo!! ' />
         </div>
     )
 }
