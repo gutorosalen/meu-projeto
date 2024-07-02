47fd6dbd98a3590649d9633427d9c1defa2ed7ca
unknown <gutorosalen.gustavorosalen@hotmail.com>
2024-07-01 21:38:46 -0300

    v1.2

--- src/componentes/section/Cards.js
+++ src/componentes/section/Cards.js
@@ -1,19 +1,31 @@
 import styles from './Cards.module.css'
 import ButtonB from './ButtonB'
-
+import { useState } from 'react'
 function Cards({ img, title, tech, description, repo, site }) {
+
+    const [info, setInfo] = useState(false)
+
+    function infoOn() {
+        setInfo(true)
+    }
+    function infoOff() {
+        setInfo(false)
+    }
+
     return (
-        <div className={styles.cards} >
-            <a href={site}>
+        <div onMouseLeave={infoOff} className={styles.cards} >
+            <a onMouseEnter={infoOn} href={site}>
                 <img src={img} alt='ERRO' />
             </a>
 
-            <section>
-                <h3>{title}</h3>
-                <p>Tecnologias:{tech}</p>
-                <p>{description}</p>
-                <ButtonB text="Acesse o Repositório" link={repo} />
-            </section>
+            {info === true && (
+                <section>
+                    <h3>{title}</h3>
+                    <p>Tecnologias:{tech}</p>
+                    <p>{description}</p>
+                    <ButtonB text="Acesse o Repositório" link={repo} />
+                </section>
+            )}
         </div>
     )
 }
--- src/componentes/section/Presentation.js
+++ src/componentes/section/Presentation.js
@@ -1,12 +1,46 @@
 import styles from './Presentation.module.css'
 import ButtonA from './ButtonA'
+import { useEffect, useState } from 'react'
 /*import ButtonB from './ButtonB'*/
 
 function Presentation() {
+    const [text, setText] = useState('');
+    const toRotate = ['Gustavo Henrique Rosalen', 'Desenvolvedor Full-Stack', 'Auxiliar e Analista de dados'];
+    const [loop, setLoop] = useState(0);
+    const [IsDeleting, setIsDeleting] = useState(false);
+    const period = [100];
+    const [Delta, setDelta] = useState(100)
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            toType()
+        }, Delta)
+        return () => { clearInterval(ticker) }
+
+    }, [text])
+
+    const toType = () => {
+        let i = loop % toRotate.length;
+        let fullText = toRotate[i]
+        let updateText = IsDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1)
+
+        setText(updateText);
+
+        if (!IsDeleting && updateText === fullText) {
+            setIsDeleting(true);
+            setDelta(period);
+        } else if (IsDeleting && updateText === '') {
+            setIsDeleting(false);
+            setDelta(period);
+            setLoop(loop + 1);
+        }
+
+    }
+
     return (
         <div className={styles.presentation} id='Presentation'>
             <p><strong>Bem-vindo ao meu Portfólio.</strong></p>
-            <h1>Olá, eu sou Gustavo.</h1>
+            <h1>Olá, eu sou {text}.</h1>
             <p>
                 Sou um apaixonado por tecnologia e soluções inovadoras.<br />
                 Como Auxiliar, Analista de T.I e Desenvolvedor full-stack, <br />
